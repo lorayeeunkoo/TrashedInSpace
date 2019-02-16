8bc5accd9c901c3e097dcd0bb8c5385d1da16d16
= <=>
2019-02-16 14:26:31 -0800

    rope added

---
+++ small_ship.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+#var rot = 0
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
