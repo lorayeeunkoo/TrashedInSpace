430690b588994100f4c44733a4632fe809d27145
lkoo <lkoo@sfu.ca>
2019-02-16 04:34:54 -0800

    commiting to pull from steven's branch

---
+++ LongPieceRope/Rope.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+var PIECE = preload("res://LongPieceRope/Piece.tscn")
+var BALL = preload("res://LongPieceRope/Ball.tscn")
+
+export (int) var pieces = 40
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	var parent = $Sprite/Anchor
+	for i in range (pieces):
+		parent = addPiece(parent)
+	addBall(parent)
+		
+func addPiece(parent):
+	var joint = parent.get_node("CollisionShape2D/Joint")
+	var piece = PIECE.instance()
+	joint.add_child(piece)
+	joint.node_a = parent.get_path()
+	joint.node_b = piece.get_path()
+	return piece
+	
+func addBall(parent):
+	var joint = parent.get_node("CollisionShape2D/Joint")
+	var ball = BALL.instance()
+	joint.add_child(ball)
+	joint.node_a = parent.get_path()
+	joint.node_b = ball.get_path()
+	return ball
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
---
+++ Sprite.gd
@@ -0,0 +1,35 @@
+extends Sprite
+
+var over = false
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+func _process(delta):
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) and over:
+		var mousePos = get_viewport().get_mouse_position()
+#		var vector = mousePos - global_position
+#		oldPos = pos
+#		pos += vector
+		position += mousePos - global_position
+
+
+func _on_Control_mouse_entered():
+	over = true
+	modulate = Color(0x0000ffff)
+
+
+func _on_Control_mouse_exited():
+	over = false
+	modulate = Color(0xffffffff)
