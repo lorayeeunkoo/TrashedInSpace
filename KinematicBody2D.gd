2eeffb8dd37e9d43eb919c818ea02a80e37a213d
Alvin Ho <ho.alvin857@gmail.com>
2019-02-16 19:54:28 -0800

    gah

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,14 +1,20 @@
 extends KinematicBody2D
 
+const UP = Vector2(0,-1)
 var motion = Vector2()
 
 func _physics_process(delta):
-	motion.y+=10
-	if Input.is_action_pressed("ui_right"):
+	motion.y += 10
+	if Input.is_action_pressed("ui_page_down"):
 		motion.x = 100
-	elif Input.is_action_pressed("ui_left"):
-		motion.x = 100
+		
+	elif Input.is_action_pressed("ui_page_up"):
+		motion.x = -100
 	else:
 		motion.x = 0
-	move_and_slide(motion)
+	
+	if is_on_floor():
+		if(Input.is_action_just_pressed("ui_accept")):
+			motion.y = -400
+	motion = move_and_slide(motion, UP)
 	pass
